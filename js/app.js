5d4dca11b4c3c6a6e47a732cc432df7f512334fe
Nils Henrar <nils.henrar@gmail.com>
2023-03-23 11:57:13 +0100

    ajout du script js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* JS */
+//sélection des éléments du DMO HTML
+const inputTask= document.forms['frmTodo'].task;
+const add= document.forms['frmTodo'].add;
+const taskList= document.getElementById['taskList'].taskList;
